f68c40436e8e5f2543fcd0606c8e0dd5865e72dd
mathinch <mathinch12@gmail.com>
2025-08-15 02:25:37 +0900

    First commit

---
+++ 27433.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int Factorial(int n){
+    if(n == 1 || n == 0){
+        return 1;
+    }
+    else{
+        return n * Factorial(n-1);
+    }
+}
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+
+    int temp;
+    temp = Factorial(N);
+
+    printf("%d",temp);
+
+    return 0;
+}
